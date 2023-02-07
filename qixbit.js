191a3db24ae0dcb1a6d74c0c272f2ef1af9f7a40
abdullng <124679489+abdullng@users.noreply.github.com>
2023-02-07 18:04:54 +0100

    Create qixbit.js

---
+++ qixbit.js
@@ -0,0 +1,12 @@
+var imageURLs = [
+       "https://helpdesk.qixbit.com/ad/banner-mob1.png"
+     , "https://helpdesk.qixbit.com/ad/banner-mob2.png"
+     , "https://helpdesk.qixbit.com/ad/banner-mob3.png"
+  ];
+  function getImageTagmob() {
+    var img = '<img src="';
+    var randomIndex = Math.floor(Math.random() * imageURLs.length);
+    img += imageURLs[randomIndex];
+    img += '\" class="qixbit-mobile" alt="Qixbit App"/>';
+    return img;
+  }
