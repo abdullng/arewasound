cb8145d29ddd5a199bbd8e3e78237d216a20ca20
abdullng <124679489+abdullng@users.noreply.github.com>
2023-02-07 18:23:38 +0100

    Update qixbit.js

--- qixbit.js
+++ qixbit.js
@@ -3,7 +3,7 @@ var imageURLs = [
      , "https://helpdesk.qixbit.com/ad/banner-mob2.png"
      , "https://helpdesk.qixbit.com/ad/banner-mob3.png"
   ];
-  function getImageTagmob() {
+  function getImageTagmob1() {
     var img = '<img src="';
     var randomIndex = Math.floor(Math.random() * imageURLs.length);
     img += imageURLs[randomIndex];
