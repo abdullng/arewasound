f0c16f1e438ec54ed6ebb95914fd26583507b7f4
abdullng <124679489+abdullng@users.noreply.github.com>
2023-03-31 05:45:29 +0100

    Update groups.js

--- groups.js
+++ groups.js
@@ -1,10 +1,10 @@
-var imageURLs = ["https://arewasound.com/groups.png",];
+var imageURLs = ["https://arewasound.com/groups1.png", "https://arewasound.com/groups2.png",];
     
   function getImageTagmob() 
   {
     var img = '<img src="';
     var randomIndex = Math.floor(Math.random() * imageURLs.length);
     img += imageURLs[randomIndex];
-    img += '\" class="qixbit-mobile" alt="Qixbit App"/>';
+    img += '\" class="qixbit-mobile" alt="Group"/>';
     return img;
   }
